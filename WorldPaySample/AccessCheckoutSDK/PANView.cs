0c417cda396be9f4f697746f3d64921a3401a3d9
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-02-16 21:29:27 +0300

    Create project for porting.

---
+++ WorldPaySample/AccessCheckoutSDK/PANView.cs
@@ -0,0 +1,57 @@
+﻿//
+// PANView.cs
+//
+// Author:
+//       Yauheni Pakala <evgeniy.pakalo@gmail.com>
+//
+// Copyright (c) 2020 Yauheni Pakala
+//
+using System;
+using System.Linq;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace AccessCheckoutSDK
+{
+    [Register(nameof(PANView))]
+    public class PANView : UIView
+    {
+        //UITextField textField;
+        //UIImageView imageView;
+
+        //public PAN? Text
+        //{
+        //    get
+        //    {
+        //        var text = textField.Text;
+        //        return text;
+        //    }
+        //}
+
+        public PANView(NSCoder coder) : base(coder)
+        {
+            setupViewFromNib();
+        }
+
+        public PANView(CGRect frame) : base(frame)
+        {
+            setupViewFromNib();
+        }
+
+        private void setupViewFromNib()
+        {
+            var bundle = NSBundle.FromClass(Class); // TODO: check
+            var nib = UINib.FromName(nameof(PANView), bundle);
+            var view = nib.Instantiate(this, null).First() as UIView;
+            view.Frame = Bounds;
+            view.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+            AddSubview(view);
+
+            //textField.Placeholder = TODO: add view
+            //textField.Delegate =
+            //textField.AddTarget
+
+        }
+    }
+}
