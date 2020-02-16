be16bd211858fa93b0d6fc3b89fd9f7aa431e934
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-02-16 21:30:02 +0300

    Add sample project.

---
+++ WorldPaySample/WorldPaySample/ViewController.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using System;
+using AccessCheckoutSDK;
+using UIKit;
+
+namespace WorldPaySample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+        }
+    }
+}
